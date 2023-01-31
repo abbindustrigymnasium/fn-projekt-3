76f62641d110d7e8d615b9c842bd2453cd72e218
abbguslof <gustav.lofqvist@abbgymnasiet.se>
2023-01-31 15:15:44 +0100

    +videos¨

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -10,6 +11,19 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+=======
+  <video autoplay loop muted id="myVideo">
+    <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4"/>
+
+  </video>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+>>>>>>> c8ffce6 (+mp4 and +lfs)
   }
 }
 </script>
@@ -23,4 +37,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+<<<<<<< HEAD
+=======
+
+#myVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+>>>>>>> c8ffce6 (+mp4 and +lfs)
 </style>
