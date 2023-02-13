24fd0578423fc4f1f4800d5c5cbfaaa8d74555c7
abbguslof <gustav.lofqvist@abbgymnasiet.se>
2023-02-13 13:44:36 +0100

    +

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,14 +5,11 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-<<<<<<< HEAD
       { path: 'Fish', component: () => import('pages/Fish.vue') },
       { path: 'Ocean', component: () => import('pages/Ocean.vue') }
-=======
       { path: 'login', component: () => import('src/pages/login.vue') },
       { path: 'signup', component: () => import('src/pages/signup.vue') }
       
->>>>>>> 44579b83d03b07cf94b2a72741a01b3646da071b
     ]
   },
 
